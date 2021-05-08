e32fa2385e3e71f089b1c65f564e39236405e7d6
willxinei <will30brabosa@yahoo.com>
2021-05-08 12:40:30 -0300

    atera controller lisstagem

--- src/modules/agendamento/infra/http/controllers/ListHorariosDisponivelController.ts
+++ src/modules/agendamento/infra/http/controllers/ListHorariosDisponivelController.ts
@@ -5,18 +5,22 @@ import { container } from "tsyringe";
 
 export default class ListHorariosDisponivelController {
    public async list(req: Request, res: Response): Promise<Response> {
-      const listHorarios = container.resolve(ListHorarioDiponilvelService);
+      try {
+         const listHorarios = container.resolve(ListHorarioDiponilvelService);
 
-      const { dia, mes, ano, service, provider_id } = req.query;
+         const { dia, mes, ano, service, provider_id } = req.query;
 
-      const list = await listHorarios.exec({
-         provider_id: String(provider_id),
-         service: String(service),
-         dia: Number(dia),
-         mes: Number(mes),
-         ano: Number(ano),
-      });
+         const list = await listHorarios.exec({
+            provider_id: String(provider_id),
+            service: String(service),
+            dia: Number(dia),
+            mes: Number(mes),
+            ano: Number(ano),
+         });
 
-      return res.json(classToClass(list));
+         return res.json(classToClass(list));
+      } catch (err) {
+         return res.json(err.message).status(400);
+      }
    }
 }
