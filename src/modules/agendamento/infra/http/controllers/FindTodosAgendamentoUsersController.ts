e91fcd81348786571f1f862ac461a43dbf4ed517
willxinei <will30barbosa@yahoo.com>
2021-08-03 06:39:55 -0300

    altera: rotas

--- src/modules/agendamento/infra/http/controllers/FindTodosAgendamentoUsersController.ts
+++ src/modules/agendamento/infra/http/controllers/FindTodosAgendamentoUsersController.ts
@@ -5,14 +5,14 @@ import { container } from "tsyringe";
 
 export default class FindTodosAgendamentosUserController {
    public async list(req: Request, res: Response): Promise<Response> {
-      // try {
-      const agenda = container.resolve(FindTodosAgendamentosUserSerice);
-      const user_id = req.user.id;
+      try {
+         const agenda = container.resolve(FindTodosAgendamentosUserSerice);
+         const user_id = req.user.id;
 
-      const ag = await agenda.execute(user_id);
-      return res.json(ag);
-      // } catch (err) {
-      //    return res.json(err.message).status(400);
-      // }
+         const ag = await agenda.execute(user_id);
+         return res.json(ag);
+      } catch (err) {
+         return res.json(err.message).status(200);
+      }
    }
 }
